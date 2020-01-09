19ffdc74a714bbb571f9743fba15b7a96fac10a7
Irina Pichugina <pichugina_i@mail.ru>
2020-01-09 18:22:44 +0000

    fetching json

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3001/items")
+      .then(resp => resp.json())
+      .then(data => {
+        let items =data.map((post, index) => {
+          return (
+            <div key={index}>
+              <h2>{post.brand}: {post.model}</h2>
+                <p>Price {post.price}</p>
+            </div>
+          )
+          })
+        this.setState({
+          isLoaded: true,
+          items: items
+        });
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+  }
+
+
+  render() {
+    const { error, isLoaded } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          {this.state.items}
+        </div>
+      )};
+  }
 }
-
-export default App;
+export default App; 
