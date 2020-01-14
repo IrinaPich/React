beb5ffc70aa54b91e530ad77aadfe1e33f3d87f7
Irina Pichugina <pichugina_i@mail.ru>
2020-01-14 16:35:18 +0000

    appended onClic for items

--- src/App.js
+++ src/App.js
@@ -6,49 +6,62 @@ class App extends Component {
     this.state = {
       error: null,
       isLoaded: false,
+      hideList: true,
       items: []
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.style = this.state.hideList ? { display: 'none' } : { display: 'block' };
   }
 
   componentDidMount() {
-    fetch("http://localhost:3001/items")
+    fetch("http://localhost:5000/items")
       .then(resp => resp.json())
-      .then(data => {
-        let items =data.map((post, index) => {
-          return (
-            <div key={index}>
-              <h2>{post.brand}: {post.model}</h2>
-                <p>Price {post.price}</p>
-            </div>
-          )
-          })
-        this.setState({
-          isLoaded: true,
-          items: items
-        });
-      },
-      (error) => {
-        this.setState({
-          isLoaded: true,
-          error
-        });
-      }
-    )
+      .then(
+        (data) => {
+          this.setState({
+            isLoaded: true,
+            items: data
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
   }
 
+  handleChange() {
+    this.setState(state => ({
+      hideList: !state.hideList
+  }));
+  }
 
   render() {
-    const { error, isLoaded } = this.state;
+    const { error, isLoaded, items } = this.state;
+
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
       return (
-        <div>
-          {this.state.items}
-        </div>
-      )};
+        <ul>{items.map((post, index) => (
+          <li key={index} onClick={this.handleChange}>{index + 1} - {post.brand}: {post.model}
+            <ul style={this.state.hideList ? { display: 'none' } : { display: 'block' }}>
+              <li>Price: {post.price}</li>
+              <li>Screen size: {post.screenSize}</li>
+              <li>Price: {post.price}</li>
+              <li>Screen width: {post.screenWidth}</li>
+              <li>Unit: {post.unit}</li>
+              <li>Weight: {post.weight}</li>
+            </ul>
+          </li>
+        ))}
+        </ul>
+      );
+    }
   }
 }
 export default App; 
