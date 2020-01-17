e20e14215a5ce927133a27c66c9a694afcb97ed6
Irina Pichugina <pichugina_i@mail.ru>
2020-01-17 10:27:11 +0000

    Implented Portal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends Component {
   constructor(props) {
@@ -7,10 +8,12 @@ class App extends Component {
       error: null,
       isLoaded: false,
       hideList: true,
-      items: []
+      items: [],
+      id: -1,
+      showWindowPortal: false,
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.style = this.state.hideList ? { display: 'none' } : { display: 'block' };
+    this.toggleWindowPortal = this.toggleWindowPortal.bind(this);
+    this.closeWindowPortal = this.closeWindowPortal.bind(this);
   }
 
   componentDidMount() {
@@ -32,14 +35,31 @@ class App extends Component {
       )
   }
 
-  handleChange() {
+  handleChange(id) {
     this.setState(state => ({
-      hideList: !state.hideList
-  }));
+      hideList: !state.hideList,
+      id: id
+    }));
+
+    window.addEventListener('beforeunload', () => {
+      this.closeWindowPortal();
+    });
+
+  }
+
+  toggleWindowPortal() {
+    this.setState(state => ({
+      ...state,
+      showWindowPortal: !state.showWindowPortal,
+    }));
+  }
+
+  closeWindowPortal() {
+    this.setState({ showWindowPortal: false})
   }
 
   render() {
-    const { error, isLoaded, items } = this.state;
+    const { error, isLoaded, items, id } = this.state;
 
     if (error) {
       return <div>Error: {error.message}</div>;
@@ -47,21 +67,107 @@ class App extends Component {
       return <div>Loading...</div>;
     } else {
       return (
-        <ul>{items.map((post, index) => (
-          <li key={index} onClick={this.handleChange}>{index + 1} - {post.brand}: {post.model}
-            <ul style={this.state.hideList ? { display: 'none' } : { display: 'block' }}>
-              <li>Price: {post.price}</li>
-              <li>Screen size: {post.screenSize}</li>
-              <li>Price: {post.price}</li>
-              <li>Screen width: {post.screenWidth}</li>
-              <li>Unit: {post.unit}</li>
-              <li>Weight: {post.weight}</li>
-            </ul>
-          </li>
-        ))}
-        </ul>
+        <div>
+
+          <ul>{items.map((post, index) => (
+            <li key={index} onClick={this.handleChange.bind(this, index)}>{index + 1} - {post.brand}: {post.model}
+              <div class="buttons">
+                <button onClick={this.toggleWindowPortal}>{this.state.showWindowPortal ? 'Close link' : 'Open link'}</button>
+              </div>
+              <ul style={this.state.hideList && (id === index) ? { display: 'block' } : { display: 'none' }}>
+                <li>Price: {post.price}</li>
+                <li>Screen size: {post.screenSize}</li>
+                <li>Price: {post.price}</li>
+                <li>Screen width: {post.screenWidth}</li>
+                <li>Unit: {post.unit}</li>
+                <li>Weight: {post.weight}</li>
+              </ul>
+            </li>
+          ))}
+          </ul>
+          {this.state.showWindowPortal && (
+            <MyWindowPortal closeWindowPortal={this.closeWindowPortal} >
+
+
+              <div>{items.map((post, index) => (
+                <ul style={(id === index) ? { display: 'block' } : { display: 'none' }}><b>{index + 1} - {post.brand}: {post.model}</b>
+                  <li>Price: {post.price}</li>
+                  <li>Screen size: {post.screenSize}</li>
+                  <li>Price: {post.price}</li>
+                  <li>Screen width: {post.screenWidth}</li>
+                  <li>Unit: {post.unit}</li>
+                  <li>Weight: {post.weight}</li>
+                </ul>
+              ))}
+              </div>
+
+              <button onClick={() => this.closeWindowPortal()} >
+                Back to results
+              </button>
+            </MyWindowPortal>
+          )}
+
+        </div>
       );
     }
   }
 }
+
+class MyWindowPortal extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    // STEP 1: create a container <div>
+    this.containerEl = document.createElement('div');
+    this.externalWindow = null;
+  }
+
+  componentDidMount() {
+    // STEP 3: open a new browser window and store a reference to it
+    this.externalWindow = window.open('', '', 'width=600,height=400,left=200,top=200');
+
+    // STEP 4: append the container <div> (that has props.children appended to it) to the body of the new window
+    this.externalWindow.document.body.appendChild(this.containerEl);
+    this.externalWindow.document.title = 'Additional info of clicked Item';
+    copyStyles(document, this.externalWindow.document);
+
+    // update the state in the parent component if the user closes the 
+    // new window
+    this.externalWindow.addEventListener('beforeunload', () => {
+      this.props.closeWindowPortal();
+    });
+  }
+
+  componentWillUnmount() {
+    // STEP 5: This will fire when this.state.showWindowPortal in the parent component becomes false
+    // So we tidy up by closing the window
+    this.externalWindow.close();
+  }
+
+  render() {
+    // STEP 2: append props.children to the container <div> that isn't mounted anywhere yet
+    return ReactDOM.createPortal(this.props.children, this.containerEl);
+  }
+}
+
+function copyStyles(sourceDoc, targetDoc) {
+  Array.from(sourceDoc.styleSheets).forEach(styleSheet => {
+    if (styleSheet.cssRules) { // for <style> elements
+      const newStyleEl = sourceDoc.createElement('style');
+
+      Array.from(styleSheet.cssRules).forEach(cssRule => {
+        // write the text of each rule into the body of the style element
+        newStyleEl.appendChild(sourceDoc.createTextNode(cssRule.cssText));
+      });
+
+      targetDoc.head.appendChild(newStyleEl);
+    } else if (styleSheet.href) { // for <link> elements loading CSS from a URL
+      const newLinkEl = sourceDoc.createElement('link');
+
+      newLinkEl.rel = 'stylesheet';
+      newLinkEl.href = styleSheet.href;
+      targetDoc.head.appendChild(newLinkEl);
+    }
+  });
+}
+
 export default App; 
